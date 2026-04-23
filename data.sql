0194aa21eb5915254b1842a00f0884be9e0386de
Akshat Singh Kshatriya <akshatsinghkshatriya@gmail.com>
2026-04-24 03:18:43 +0530

    Add files via upload

---
+++ data.sql
@@ -0,0 +1,14 @@
+CREATE TABLE equity_features (
+    "Date" TIMESTAMP,
+    "Open" FLOAT,
+    "High" FLOAT,
+    "Low" FLOAT,
+    "Close" FLOAT,
+    "Volume" BIGINT,
+    "Ticker" TEXT,
+    "Daily_Return" FLOAT,
+    "SMA_20" FLOAT,
+    "SMA_50" FLOAT,
+    "Volatility_20d" FLOAT,
+    "Target_Next_Day_Return" FLOAT
+);
